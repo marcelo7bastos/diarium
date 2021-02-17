7362d4a649b191940d9c08a295c3625f2510f52c
marcelo7bastos <marceloc.bastos@hotmail.com>
2021-02-17 10:38:36 -0300

    versão de teste 0.2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return<div>DIARIUM - PRIMEIRA VERSÃO (0.1) - MARCELO CABREIRA BASTOS</div>
+    return<div>DIARIUM - PRIMEIRA VERSÃO (0.1) - MARCELO CABREIRA BASTOS
+        Passos para &quot;commitar&quot;:
+1 - git add -A
+2 - git commit -m &quot;vers&atilde;o de teste 0.1&quot;
+3 - git push https://github.com/marcelo7bastos/diarium.git main
+    </div>
 }
 
 export default Home
